c919bd98bcfd9327ddc71852549ca493f1ac56c9
blackwhitebear8 <45701225+blackwhitebear8@users.noreply.github.com>
2024-08-02 19:46:27 +0200

    Update generate.go
    Sort channels by tvg-id

--- m3u/generate.go
+++ m3u/generate.go
@@ -8,9 +8,12 @@ import (
 	"m3u-stream-merger/utils"
 	"net/http"
 	"net/url"
+	"sort"
+	"strconv"
 	"strings"
 )
 
+// getFileExtensionFromUrl haalt de bestandsextensie op van een URL
 func getFileExtensionFromUrl(rawUrl string) (string, error) {
 	u, err := url.Parse(rawUrl)
 	if err != nil {
@@ -20,9 +23,10 @@ func getFileExtensionFromUrl(rawUrl string) (string, error) {
 	if pos == -1 {
 		return "", errors.New("couldn't find a period to indicate a file extension")
 	}
-	return u.Path[pos+1 : len(u.Path)], nil
+	return u.Path[pos+1:], nil
 }
 
+// GenerateStreamURL genereert een stream URL
 func GenerateStreamURL(baseUrl string, title string, sampleUrl string) string {
 	ext, err := getFileExtensionFromUrl(sampleUrl)
 	if err != nil {
@@ -31,6 +35,7 @@ func GenerateStreamURL(baseUrl string, title string, sampleUrl string) string {
 	return fmt.Sprintf("%s/%s.%s\n", baseUrl, utils.GetStreamUID(title), ext)
 }
 
+// GenerateM3UContent genereert de M3U-inhoud
 func GenerateM3UContent(w http.ResponseWriter, r *http.Request, db *database.Instance) {
 	streams, err := db.GetStreams()
 	if err != nil {
@@ -52,19 +57,30 @@ func GenerateM3UContent(w http.ResponseWriter, r *http.Request, db *database.Ins
 		log.Println(fmt.Errorf("Fprintf error: %v", err))
 	}
 
+	// Sorteer de streams op basis van TVG-ID, interpreteer de ID's als getallen
+	sort.Slice(streams, func(i, j int) bool {
+		id1, err1 := strconv.Atoi(streams[i].TvgID)
+		id2, err2 := strconv.Atoi(streams[j].TvgID)
+		if err1 != nil || err2 != nil {
+			// Als een van de conversies mislukt, vergelijk dan als strings
+			return streams[i].TvgID < streams[j].TvgID
+		}
+		return id1 < id2
+	})
+
 	for _, stream := range streams {
 		if len(stream.URLs) == 0 {
 			continue
 		}
 
-		// Write #EXTINF line
+		// Schrijf de #EXTINF regel
 		_, err := fmt.Fprintf(w, "#EXTINF:-1 channelID=\"x-ID.%s\" tvg-chno=\"%s\" tvg-id=\"%s\" tvg-name=\"%s\" tvg-logo=\"%s\" group-title=\"%s\",%s\n",
 			stream.TvgID, stream.TvgID, stream.TvgID, stream.Title, stream.LogoURL, stream.Group, stream.Title)
 		if err != nil {
 			continue
 		}
 
-		// Write stream URL
+		// Schrijf de stream URL
 		_, err = fmt.Fprintf(w, "%s", GenerateStreamURL(baseUrl, stream.Title, stream.URLs[0].Content))
 		if err != nil {
 			continue
