a4ceb7cc298620c0d73223f3009e81c7360baea7
Son Roy Almerol <github@snry.me>
2024-08-19 20:31:28 -0400

    use m3u index for load balancer

--- stream_handler.go
+++ stream_handler.go
@@ -15,13 +15,7 @@ import (
 )
 
 func loadBalancer(stream database.StreamInfo, previous int) (*http.Response, string, int, error) {
-	m3uIndexes := make([]int, 0, len(stream.URLs))
-	for k := range stream.URLs {
-		if k == previous {
-			continue
-		}
-		m3uIndexes = append(m3uIndexes, k)
-	}
+	m3uIndexes := utils.GetM3UIndexes()
 
 	sort.Slice(m3uIndexes, func(i, j int) bool {
 		return db.ConcurrencyPriorityValue(i) > db.ConcurrencyPriorityValue(j)
@@ -34,10 +28,19 @@ func loadBalancer(stream database.StreamInfo, previous int) (*http.Response, str
 		allSkipped := true // Assume all URLs might be skipped
 
 		for _, index := range m3uIndexes {
-			url := stream.URLs[index]
+			if index == previous {
+				log.Printf("Skipping M3U_%d: marked as previous stream\n", index+1)
+				continue
+			}
+
+			url, ok := stream.URLs[index]
+			if !ok {
+				log.Printf("Channel not found from M3U_%d: %s\n", index+1, stream.Title)
+				continue
+			}
 
 			if db.CheckConcurrency(index) {
-				log.Printf("Concurrency limit reached for M3U_%d: %s", index+1, url)
+				log.Printf("Concurrency limit reached for M3U_%d: %s\n", index+1, url)
 				continue
 			}
 
@@ -51,7 +54,7 @@ func loadBalancer(stream database.StreamInfo, previous int) (*http.Response, str
 		}
 
 		if allSkipped {
-			break // Break early if all URLs are skipped due to concurrency
+			break
 		}
 
 		lap++
@@ -126,55 +129,55 @@ func streamHandler(w http.ResponseWriter, r *http.Request, db *database.Instance
 		return
 	}
 
-	resp, selectedUrl, selectedIndex, err := loadBalancer(stream, -1)
-	if err != nil {
-		log.Printf("Error fetching initial stream for %s: %v\n", streamSlug, err)
-		http.Error(w, "Error fetching stream. Exhausted all streams.", http.StatusInternalServerError)
-		return
-	}
-
-	for k, v := range resp.Header {
-		if strings.ToLower(k) != "content-length" {
-			for _, val := range v {
-				w.Header().Set(k, val)
-			}
-		}
-	}
-
-	w.Header().Set("Cache-Control", "no-cache")
-	w.Header().Set("Access-Control-Allow-Origin", "*")
+	currentIndex := -1
+	var resp *http.Response
 
 	for {
 		select {
 		case <-ctx.Done():
 			log.Printf("Client disconnected: %s\n", r.RemoteAddr)
-			resp.Body.Close()
+			if resp != nil {
+				resp.Body.Close()
+			}
 			return
 		default:
+			var selectedUrl string
+			var selectedIndex int
+
+			resp, selectedUrl, selectedIndex, err = loadBalancer(stream, currentIndex)
+			if err != nil {
+				log.Printf("Error reloading stream for %s: %v\n", streamSlug, err)
+				http.Error(w, "Error fetching stream. Exhausted all streams.", http.StatusInternalServerError)
+				return
+			}
+
+			// HTTP header initialization
+			if currentIndex == -1 {
+				w.Header().Set("Cache-Control", "no-cache")
+				w.Header().Set("Access-Control-Allow-Origin", "*")
+				for k, v := range resp.Header {
+					if strings.ToLower(k) != "content-length" {
+						for _, val := range v {
+							w.Header().Set(k, val)
+						}
+					}
+				}
+			}
 			exitStatus := make(chan int)
 
-			currentUrl := selectedUrl
-			currentIndex := selectedIndex
-			currentResp := resp
+			currentIndex = selectedIndex
 
-			log.Printf("Proxying %s to %s\n", r.RemoteAddr, currentUrl)
+			log.Printf("Proxying %s to %s\n", r.RemoteAddr, selectedUrl)
 			go func(m3uIndex int, resp *http.Response, r *http.Request, w http.ResponseWriter, exitStatus chan int) {
 				proxyStream(m3uIndex, resp, r, w, exitStatus)
-			}(currentIndex, currentResp, r, w, exitStatus)
+			}(currentIndex, resp, r, w, exitStatus)
 
 			streamExitCode := <-exitStatus
-			log.Printf("Exit code %d received from %s\n", streamExitCode, currentUrl)
+			log.Printf("Exit code %d received from %s\n", streamExitCode, selectedUrl)
 
 			if streamExitCode == 1 {
 				// Retry on server-side connection errors
-				log.Printf("Server connection failed: %s\n", currentUrl)
 				log.Printf("Retrying other servers...\n")
-				resp, selectedUrl, selectedIndex, err = loadBalancer(stream, currentIndex)
-				if err != nil {
-					log.Printf("Error reloading stream for %s: %v\n", streamSlug, err)
-					http.Error(w, "Error fetching stream. Exhausted all streams.", http.StatusInternalServerError)
-					return
-				}
 			} else {
 				// Consider client-side connection errors as complete closure
 				log.Printf("Client has closed the stream: %s\n", r.RemoteAddr)
