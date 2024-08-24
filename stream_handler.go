55de282244e8ab269984f5f8e4fcda50c1270859
Son Roy Almerol <github@snry.me>
2024-08-24 18:21:29 -0400

    add timeout duration before stream is considered down

--- stream_handler.go
+++ stream_handler.go
@@ -13,6 +13,7 @@ import (
 	"sort"
 	"strconv"
 	"strings"
+	"time"
 )
 
 func loadBalancer(stream database.StreamInfo, previous *[]int) (*http.Response, string, int, error) {
@@ -96,27 +97,61 @@ func proxyStream(m3uIndex int, resp *http.Response, r *http.Request, w http.Resp
 		buffer = make([]byte, bufferMbInt*1024*1024)
 	}
 
+	// Set a timeout duration
+	timeoutSecond, err := strconv.Atoi(os.Getenv("STREAM_TIMEOUT"))
+	if err != nil || timeoutSecond <= 0 {
+		timeoutSecond = 3
+	}
+
+	timeoutDuration := time.Duration(timeoutSecond) * time.Second
+	timer := time.NewTimer(timeoutDuration)
+	defer timer.Stop()
+
+	returnStatus := 0
+
 	for {
-		n, err := resp.Body.Read(buffer)
-		if err != nil {
-			if err == io.EOF {
-				log.Printf("Stream ended (EOF reached): %s\n", r.RemoteAddr)
-				statusChan <- 2
-				return
+		select {
+		case <-timer.C:
+			// Timer expired
+			log.Printf("Timeout reached while trying to stream: %s\n", r.RemoteAddr)
+			statusChan <- returnStatus
+			return
+		default:
+			n, err := resp.Body.Read(buffer)
+			if err != nil {
+				if err == io.EOF {
+					log.Printf("Stream ended (EOF reached): %s\n", r.RemoteAddr)
+					if utils.IsPlaylistFile(r.RemoteAddr) {
+						statusChan <- 2
+						return
+					}
+					returnStatus = 2
+					log.Printf("Retrying same stream until timeout (%d seconds) is reached...\n", timeoutDuration)
+					continue
+				}
+				log.Printf("Error reading stream: %s\n", err.Error())
+				returnStatus = 1
+				log.Printf("Retrying same stream until timeout (%d seconds) is reached...\n", timeoutDuration)
+				continue
 			}
-			log.Printf("Error reading stream: %s\n", err.Error())
-			statusChan <- 1
-			return
-		}
 
-		if _, err := w.Write(buffer[:n]); err != nil {
-			log.Printf("Error writing to response: %s\n", err.Error())
-			statusChan <- 0
-			return
-		}
+			if _, err := w.Write(buffer[:n]); err != nil {
+				log.Printf("Error writing to response: %s\n", err.Error())
+				returnStatus = 0
+				log.Printf("Retrying same stream until timeout (%d seconds) is reached...\n", timeoutDuration)
+				continue
+			}
 
-		if flusher, ok := w.(http.Flusher); ok {
-			flusher.Flush()
+			if flusher, ok := w.(http.Flusher); ok {
+				flusher.Flush()
+			}
+
+			// Reset the timer on each successful write
+			if !timer.Stop() {
+				<-timer.C // Drain the channel if timer expired
+			}
+
+			timer.Reset(timeoutDuration)
 		}
 	}
 }
