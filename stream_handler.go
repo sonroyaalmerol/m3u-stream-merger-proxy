e395cdcf83225c72fddb79820b4ad1b0e982c292
Son Roy Almerol <github@snry.me>
2024-08-25 19:54:05 -0400

    prevent retry for x-mpegurl instead

--- stream_handler.go
+++ stream_handler.go
@@ -140,7 +140,7 @@ func proxyStream(ctx context.Context, m3uIndex int, resp *http.Response, r *http
 			if err != nil {
 				if err == io.EOF {
 					log.Printf("Stream ended (EOF reached): %s\n", r.RemoteAddr)
-					if strings.ToLower(resp.Header.Get("Content-Type")) != "video/mp2t" {
+					if strings.ToLower(resp.Header.Get("Content-Type")) == "application/x-mpegurl" {
 						statusChan <- 2
 						return
 					}
@@ -283,8 +283,8 @@ func streamHandler(w http.ResponseWriter, r *http.Request, db *database.Instance
 			streamExitCode := <-exitStatus
 			log.Printf("Exit code %d received from %s\n", streamExitCode, selectedUrl)
 
-			if streamExitCode == 2 && strings.ToLower(resp.Header.Get("Content-Type")) != "video/mp2t" {
-				log.Printf("Successfully proxied file: %s\n", r.RemoteAddr)
+			if streamExitCode == 2 && strings.ToLower(resp.Header.Get("Content-Type")) == "application/x-mpegurl" {
+				log.Printf("Successfully proxied playlist: %s\n", r.RemoteAddr)
 				cancel()
 			} else if streamExitCode == 1 || streamExitCode == 2 {
 				// Retry on server-side connection errors
