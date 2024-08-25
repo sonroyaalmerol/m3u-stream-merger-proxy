39dbd351648232275e23cfcac4c012d3f57b5fc2
Son Roy Almerol <github@snry.me>
2024-08-24 21:48:11 -0400

    draing http body on close

--- stream_handler.go
+++ stream_handler.go
@@ -70,6 +70,7 @@ func loadBalancer(stream database.StreamInfo, previous *[]int) (*http.Response, 
 				log.Printf("[DEBUG] Error fetching stream from %s: %s\n", url, err.Error())
 			}
 
+			io.Copy(io.Discard, resp.Body)
 			resp.Body.Close()
 		}
 
@@ -246,6 +247,7 @@ func streamHandler(w http.ResponseWriter, r *http.Request, db *database.Instance
 				cancel()
 			}
 
+			io.Copy(io.Discard, resp.Body)
 			resp.Body.Close()
 		}
 	}
