cc7b63cb4de91848b256ec5e4991b07677f72748
Son Roy Almerol <github@snry.me>
2024-09-22 22:39:14 -0400

    git add .

--- proxy/buffer.go
+++ proxy/buffer.go
@@ -14,8 +14,9 @@ type Buffer struct {
 	clients         map[int]chan []byte
 	clientPositions map[int]int
 	clientNextId    int
-	ingest          sync.Mutex
+	clientMu        map[int]*sync.Mutex
 	mu              sync.Mutex
+	ingest          sync.Mutex
 }
 
 // TODO: move buffers to Redis
@@ -26,12 +27,16 @@ func NewBuffer() *Buffer {
 		data:            []byte{},
 		clients:         make(map[int]chan []byte),
 		clientPositions: make(map[int]int),
+		clientMu:        make(map[int]*sync.Mutex),
 		testedIndexes:   []int{},
 	}
 	return buf
 }
 
 func (b *Buffer) Write(data []byte) {
+	b.mu.Lock()
+	defer b.mu.Unlock()
+
 	b.data = append(b.data, data...)
 }
 
@@ -54,6 +59,31 @@ func (b *Buffer) Subscribe(ctx context.Context) chan []byte {
 		for {
 			select {
 			case <-ctx.Done():
+				return
+			default:
+				b.clientMu[clientID].Lock()
+
+				if len(b.data) > maxBufferSize {
+					trimSize := len(b.data) - maxBufferSize
+
+					b.data = b.data[trimSize:]
+
+					if b.clientPositions[clientID] < trimSize {
+						b.clientPositions[clientID] = 0
+					} else {
+						b.clientPositions[clientID] -= trimSize
+					}
+				}
+
+				b.clientMu[clientID].Unlock()
+			}
+		}
+	}()
+
+	go func() {
+		for {
+			select {
+			case <-ctx.Done():
 				if ch, exists := b.clients[clientID]; exists {
 					close(ch) // close the channel when unsubscribing
 					delete(b.clients, clientID)
@@ -65,6 +95,8 @@ func (b *Buffer) Subscribe(ctx context.Context) chan []byte {
 
 				return
 			default:
+				b.clientMu[clientID].Lock()
+
 				pos, ok := b.clientPositions[clientID]
 				if !ok {
 					pos = 0
@@ -73,36 +105,11 @@ func (b *Buffer) Subscribe(ctx context.Context) chan []byte {
 				if len(b.data) >= bufferSize {
 					chunk := b.data[pos : pos+bufferSize]
 
-					// Send the chunk to the client
 					b.clients[clientID] <- chunk
-
-					// Update the client's position after sending the chunk
 					b.clientPositions[clientID] += bufferSize
-
-					// Check if the buffer exceeds the maximum allowed size
-					if len(b.data) > maxBufferSize {
-						b.mu.Lock()
-
-						// Calculate how much we need to trim from the buffer
-						trimSize := len(b.data) - maxBufferSize
-
-						// Remove the oldest `trimSize` data from the buffer
-						b.data = b.data[trimSize:]
-
-						// Adjust all clients' positions since the buffer was trimmed
-						for id, pos := range b.clientPositions {
-							if pos < trimSize {
-								// Client is behind the trimmed portion, adjust it to the new start of the buffer
-								b.clientPositions[id] = 0
-							} else {
-								// Adjust the client's position relative to the new buffer start
-								b.clientPositions[id] -= trimSize
-							}
-						}
-
-						b.mu.Unlock()
-					}
 				}
+
+				b.clientMu[clientID].Unlock()
 			}
 		}
 	}()
