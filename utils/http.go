f8167ca09992aaeb292bf0d472cee50c0dd1efbf
Son Roy Almerol <github@snry.me>
2024-10-09 17:25:26 -0400

    ensure user-agent header is set on redirects

--- utils/http.go
+++ utils/http.go
@@ -11,7 +11,13 @@ func CustomHttpRequest(method string, url string) (*http.Response, error) {
 	userAgent := GetEnv("USER_AGENT")
 
 	// Create a new HTTP client with a custom User-Agent header
-	client := &http.Client{}
+	client := &http.Client{
+		CheckRedirect: func(req *http.Request, via []*http.Request) error {
+			// Follow redirects while preserving the custom User-Agent header
+			req.Header.Set("User-Agent", userAgent)
+			return nil
+		},
+	}
 
 	req, err := http.NewRequest(method, url, nil)
 	if err != nil {
