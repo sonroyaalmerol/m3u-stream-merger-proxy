750565b868067999c450851d1ce87ef668d65b55
Son Roy Almerol <sralmerol@sgl.com>
2024-11-18 14:20:17 -0500

    fix load balancer

--- proxy/load_balancer.go
+++ proxy/load_balancer.go
@@ -58,13 +58,11 @@ func (instance *StreamInstance) LoadBalancer(previous *[]int, method string) (*h
 				continue
 			}
 
-			urlLine, ok := instance.Info.URLs[index]
+			url, ok := instance.Info.URLs[index]
 			if !ok {
 				utils.SafeLogf("Channel not found from M3U_%d: %s\n", index+1, instance.Info.Title)
 				continue
 			}
-
-			url := store.GetURL(index, urlLine)
 
 			if instance.Cm.CheckConcurrency(index) {
 				utils.SafeLogf("Concurrency limit reached for M3U_%d: %s\n", index+1, url)
