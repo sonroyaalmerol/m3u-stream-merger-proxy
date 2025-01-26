77efb9df6e915aedbb10d3950a5fa57c346599ed
Kevin Pirnie <kpirnie75@gmail.com>
2025-01-26 11:32:55 -0500

    more stream cancellation work for better memory management

--- handlers/ffmpeg_handler.go
+++ handlers/ffmpeg_handler.go
@@ -23,45 +23,19 @@ func FfmpegHandler(w http.ResponseWriter, r *http.Request, _url string, resp *ht
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Set("Transfer-Encoding", "chunked")
 
-	if debug {
-		utils.SafeLogf("[DEBUG] Setting up: %v\n", _url)
-	}
-
 	// setup the command to run
 	_cmd_args := fmt.Sprintf(
 		"%s -i pipe:0 -c copy -copyts -f mpegts %s pipe:1",
 		_ffm_input,
 		_ffm_output )
 
+	// debug
 	if debug {
 		utils.SafeLogf("[DEBUG] FFMpeg CMD Args: %v\n", _cmd_args)
 	}
 
 	// Prepare FFmpeg command
 	cmd := exec.Command("/usr/local/bin/ffmpeg", strings.Fields( _cmd_args )[0:]... )
-
-	// Get a pipe to the stdin of the ffmpeg process
-	cmdIn, err := cmd.StdinPipe()
-	if err != nil {
-		utils.SafeLogf("Error creating stdin pipe: %v\n", err)
-		statusChan <- 5
-		cmd.Process.Kill( ) 
-	}
-
-	// Set up a pipe to capture FFmpeg's output
-	cmdOut, err := cmd.StdoutPipe()
-	if err != nil {
-		utils.SafeLogf("Error creating stdout pipe: %v", err)
-		statusChan <- 5
-		cmd.Process.Kill( )
-	}
-
-	// Start FFmpeg process
-	if err := cmd.Start(); err != nil {
-		utils.SafeLogf("Error starting FFmpeg: %v", err)
-		statusChan <- 5
-		cmd.Process.Kill( )
-	}
 
 	// Cancel the process if the client disconnects
 	ctx, cancel := context.WithCancel(r.Context())
@@ -72,12 +46,40 @@ func FfmpegHandler(w http.ResponseWriter, r *http.Request, _url string, resp *ht
 		utils.SafeLogf("Client disconnected, FFmpeg process killed")
 	}()
 
+	// Get a pipe to the stdin of the ffmpeg process
+	cmdIn, err := cmd.StdinPipe()
+	if err != nil {
+		utils.SafeLogf("Error creating stdin pipe: %v\n", err)
+		statusChan <- 5 
+		cancel() // Cancel context to stop processing
+		return
+	}
+
+	// Set up a pipe to capture FFmpeg's output
+	cmdOut, err := cmd.StdoutPipe()
+	if err != nil {
+		utils.SafeLogf("Error creating stdout pipe: %v", err)
+		statusChan <- 5 
+		cancel() // Cancel context to stop processing
+		return
+	}
+
+	// Start FFmpeg process
+	if err := cmd.Start(); err != nil {
+		utils.SafeLogf("Error starting FFmpeg: %v", err)
+		statusChan <- 5 
+		cancel() // Cancel context to stop processing
+		return
+	}
+
 	// Stream data from the remote source to FFmpeg's stdin
 	go func() {
 		defer cmdIn.Close()
 		if _, err := io.Copy(cmdIn, resp.Body); err != nil {
 			utils.SafeLogf("Error streaming to FFmpeg: %v\n", err)
+			statusChan <- 5 
 			cancel() // Cancel context to stop processing
+			return
 		}
 	}()
 	
@@ -89,13 +91,13 @@ func FfmpegHandler(w http.ResponseWriter, r *http.Request, _url string, resp *ht
 	}
 
 	// Stream FFmpeg's output to the HTTP response
-	//buffer := make([]byte, 16384) // Increased buffer size: 1024, 2048, 4096, 8192
 	for {
 
 		// In the function:
 		buffer := bufferPool.Get().([]byte) // Fetch buffer from pool
 		defer bufferPool.Put(buffer)       // Return buffer to pool when done
 
+		// read the ouput buffer
 		n, err := cmdOut.Read(buffer)
 		if err == io.EOF {
 			statusChan <- 2 // End of stream
@@ -104,9 +106,11 @@ func FfmpegHandler(w http.ResponseWriter, r *http.Request, _url string, resp *ht
 		if err != nil {
 			utils.SafeLogf("Error reading FFmpeg output: %v", err)
 			statusChan <- 5 
+			cancel() // Cancel context to stop processing
 			break
 		}
 
+		// write the response buffer
 		_, writeErr := w.Write(buffer[:n])
 		if writeErr != nil {
 			utils.SafeLogf("Error writing to response: %v", writeErr)
@@ -122,52 +126,11 @@ func FfmpegHandler(w http.ResponseWriter, r *http.Request, _url string, resp *ht
 
 	}
 
-	/*
-	// Stream data from the remote source to ffmpeg's stdin
-	go func() {
-		defer cmdIn.Close()
-		if _, err := io.Copy(cmdIn, resp.Body); err != nil {
-			utils.SafeLogf("Error streaming to ffmpeg: %v\n", err)
-			statusChan <- 5
-			cmd.Process.Kill( )
-		}
-	}()
-
-	// Stream the data from FFmpeg's output to the HTTP response in chunks
-	buffer := make([]byte, 8192) // Set buffer size: was 4096
-	for {
-		n, err := cmdOut.Read(buffer)
-		if err == io.EOF {
-			statusChan <- 2 // End of stream
-			cmd.Process.Kill( )
-		}
-		if err != nil {
-			utils.SafeLogf("Error reading FFmpeg output: %v", err)
-			statusChan <- 5
-			cmd.Process.Kill( )
-		}
-
-		// Write the chunk to the HTTP response
-		_, writeErr := w.Write(buffer[:n])
-		if writeErr != nil {
-			utils.SafeLogf("Error writing to response: %v", writeErr)
-			statusChan <- 5
-			cmd.Process.Kill( )
-		}
-
-		// Ensure the client gets data in real-time by flushing the response
-		if f, ok := w.(http.Flusher); ok {
-			f.Flush()
-		}
-
-	}
-	*/
-
 	// Wait for FFmpeg to finish processing
 	if err := cmd.Wait(); err != nil {
 		utils.SafeLogf("FFmpeg exited with error: %v", err)
 		statusChan <- 5
-		cmd.Process.Kill( )
+		return
 	}
 
 }
